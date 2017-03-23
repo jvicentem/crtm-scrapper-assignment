2bd3814ddcd4fb3adf3ec3297673a5d786d0da84
jvicentem <j.vicentem@alumnos.urjc.es>
2017-03-23 15:55:35 +0100

    Wrong uri

--- rdf_graph_generator/rdf_crtm_lines_generator.py
+++ rdf_graph_generator/rdf_crtm_lines_generator.py
@@ -1,5 +1,5 @@
 from rdflib import URIRef, Literal, Graph
-from rdflib.namespace import RDF, FOAF
+
 import csv
 
 '''
@@ -30,7 +30,7 @@ def crtm_csv_to_rdf(csv_file_path, output_xml_path):
             transport = URIRef(specific_transport_uri + '.aspx')
             transportmean_name = Literal(row['transportmean_name'])
 
-            line = URIRef(specific_transport_uri + '/lineas/%d__%d___.aspx' % (transport_number, int(row['stop_code'])))
+            line = URIRef(specific_transport_uri + '/lineas/%d__%s___.aspx' % (transport_number, row['line_number']))
             line_number = Literal(row['line_number'])
 
             station = URIRef(specific_transport_uri + '/estaciones/%d_%d.aspx' % (transport_number, int(row['stop_code'])))
@@ -86,6 +86,13 @@ def crtm_csv_to_rdf(csv_file_path, output_xml_path):
             g.add((station, URIRef('http://dbpedia.org/ontology/isHandicappedAccessible'), wheelchair_boarding))
 
             # a transport mean line has stations
-            g.add((line, URIRef(specific_transport_uri + '/estaciones.aspx'), station))
+            g.add((line, URIRef(specific_transport_uri), station))
 
         g.serialize(destination=output_xml_path, format='xml')
+
+
+if __name__ == "__main__":
+    CSV_FILE_PATH = 'info_trans.csv'
+    RDF_XML_PATH = 'crtm-rdf-graph.xml'
+
+    crtm_csv_to_rdf(CSV_FILE_PATH, RDF_XML_PATH)
